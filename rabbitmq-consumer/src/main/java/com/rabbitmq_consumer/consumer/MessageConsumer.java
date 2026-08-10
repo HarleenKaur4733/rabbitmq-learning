207e0d7ce175775cef44495770f529ade6de6ef5
Harleen <harlin7814@gmail.com>
2026-08-10 23:20:25 +0530

    Consumer added

---
+++ rabbitmq-consumer/src/main/java/com/rabbitmq_consumer/consumer/MessageConsumer.java
@@ -0,0 +1,14 @@
+package com.rabbitmq_consumer.consumer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    @RabbitListener(queues = "hello.queue")
+    public void receiveMessage(String message) {
+
+        System.out.println("Received: " + message);
+    }
+}
